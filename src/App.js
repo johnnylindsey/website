b467c8379e8e655742f72ddaca3d5ca481956af8
Johnny <stormyjmaster@gmail.com>
2021-12-08 02:08:49 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import react from 'react';
 import './App.css';
+import {useState} from "react";
 
 function App() {
 
   const name = "Johnny Lindsey";
+  let [counter, setCounter] = useState(0);
+
+  function incCounter() {
+    setCounter(counter + 1);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>{name}</h1>
         <p>ill make this website soon</p>
+        <br/>
+        <button onClick={incCounter}>tap to say hi</button>
+        <p>Hi count: {counter}</p>
       </header>
     </div>
   );
