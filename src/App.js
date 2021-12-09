db9c786b53624e0fc6bf411ef89c352a1ef61080
Johnny <stormyjmaster@gmail.com>
2021-12-08 21:20:00 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import {useState} from "react";
 
 function App() {
 
-  const name = "Johnny Lindsey";
-  let [counter, setCounter] = useState(0);
+  let clickCount = 0;
+  let [txt, setTxt] = useState("Johnny Lindsey");
+  let [click, setClick] = useState("tap here");
 
-  function incCounter() {
-    setCounter(counter + 1);
+  const phrases = ["is the best", "is amazing", "loves cookout", "is truly the best", "knows absolutely nothing", "can cook", "is Italian", "built this site", "loves rihanna",
+                  "plays minecraft occasionally", "will finish this by xmas", "can no longer listen to travis scott", "loves kid cudi", "goes to uva", "owns a hamster",
+                  "is moving to san francisco", "is bad at c++", "is struggling to come up with phrases", "prefers chicken parmesan", "prefers rigatoni", "is doing homework",
+                  "likes pho", "likes korean food", "likes empanadas", "is tired", "is sad", "is happy", "drank a red bull"];
+
+  function changeText() {
+    clickCount++;
+
+    if(clickCount >= 1 && click !== "tap here again"){
+      setClick("tap here again");
+    }
+
+    let rand = Math.floor(Math.random() * phrases.length);
+    
+    setTxt("Johnny Lindsey " + phrases[rand]);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{name}</h1>
-        <p>ill make this website soon</p>
-        <br/>
-        <button onClick={incCounter}>tap to say hi</button>
-        <p>Hi count: {counter}</p>
+        <h1>{txt}</h1>
+        <a href="#" className="button1" onClick={changeText}>{click}</a>
       </header>
+
+      
     </div>
   );
 }
